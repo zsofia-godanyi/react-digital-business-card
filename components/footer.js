b66118550825ab7722b720a50780eb2b1c19adc3
zsofia-godanyi <godanyi.zsofi@gmail.com>
2022-11-29 11:20:09 +0100

    adjust container and links

--- components/footer.js
+++ components/footer.js
@@ -4,13 +4,13 @@ function Footer () {
     return (
         <footer>
             <div className="footer--icon-container">
-                <a href="https://www.linkedin.com/in/zsofia-godanyi" className="footer--btn btn">
+                <a href="https://linkedin.com/in/zsofia-godanyi" className="footer--btn btn">
                     <i className="fa fa-linkedin"></i>
                 </a>     
                 <a href="https://github.com/zsofia-godanyi" className="footer--btn btn">
                     <i className="fa fa-github"></i>
                 </a>
-                <a href="https://www.facebook.com/godanyi.zsofi" className="footer--btn btn">
+                <a href="https://facebook.com/godanyi.zsofi" className="footer--btn btn">
                     <i className="fa fa-facebook"></i>
                 </a>
             </div>
